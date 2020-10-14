c62a4edc8da68ffd6b2008446aa4f0f1959ceabb
jonasdevzero <jonasdevzero@gmail.com>
2020-10-14 03:08:50 -0300

    Adding and using Yup to validade the data

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import OrphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
     async index(req: Request, res: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
         const orphanages = await orphanagesRepository.find({
-            relations:  ['images']
+            relations: ['images']
         });
 
         return res.json(OrphanageView.renderMany(orphanages));
@@ -43,7 +44,7 @@ export default {
             return { path: image.filename }
         })
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -52,7 +53,26 @@ export default {
             openning_hours,
             open_on_weekends,
             images
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instruction: Yup.string().required(),
+            openning_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const orphanage = orphanagesRepository.create(data);
 
         await orphanagesRepository.save(orphanage);
 
--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
-import e, { ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidateErrors {
+    [key: string]: string[];
+};
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+    if (error instanceof ValidationError) {
+        let errors: ValidateErrors = {}
+
+        error.inner.forEach(err => {
+            errors[err.path] = err.errors;
+        });
+
+        return res.status(400).json({ message: 'Validate fails', errors });
+    };
+
     console.log(error);
 
     return res.status(500).json({ message: 'Internal server error' });
