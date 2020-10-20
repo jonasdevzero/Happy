5a9d51aaaba39fce9182c23609c3baab02f6a619
jonasdevzero <jonasdevzero@gmail.com>
2020-10-20 15:16:07 -0300

    rebuilding userAuth

--- web/src/hooks/useAuth.ts
+++ web/src/hooks/useAuth.ts
@@ -1,19 +1,25 @@
 import { useState, useEffect } from 'react';
 import api from '../services/api';
 
-export default function useAuth() {
-    const [user, setUser] = useState({});
+interface User {
+    name: string,
+    id: number;
+    email: string;
+    token: string;
+};
+
+export function useAuth() {
+    const [user, setUser] = useState<User>()
 
     useEffect(() => {
         const token = JSON.parse(localStorage.getItem('token') as string);
-
         if (token) {
-            api.post('/user/auth', { token })
-                .then(resp => {
-                    setUser(resp.data.user)
-                })
+            api.post('/user/auth', { token }).then(response => {
+                const { user: { id, name, email } } = response.data;
+                setUser({ id, name, email, token });
+            })
         }
-    }, [])
+    }, []);
 
-    return { user, setUser }
-}
+    return { user, setUser };
+};
