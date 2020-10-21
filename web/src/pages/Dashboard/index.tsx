cfb76696d67a655d09ea38f11361807215649e31
jonasdevzero <jonasdevzero@gmail.com>
2020-10-21 17:19:22 -0300

    Starting Dashboard page

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,15 +1,126 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-import { Sidebar } from '../../components'
-import { Container } from './styles';
+import { MiniMap, Sidebar } from '../../components';
+
+import {
+    Container,
+    Content,
+    OrphanagesContainer,
+    OrphanagesContent,
+    TitleContainer,
+    Title,
+    OrphanagesTotal,
+} from './styles';
+import { FiMapPin, FiPower, FiAlertCircle } from 'react-icons/fi';
+
+import { UserContext } from '../../contexts/UserContext';
+
+import api from '../../services/api';
+import { AxiosRequestConfig } from 'axios';
+
+interface Orphanage {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+};
 
 function Dashboard() {
+    const { setUser } = useContext(UserContext);
+    const [orphanagesContainer, setOrphanagesContainer] = useState(0); // 0 -> registered orphanages, 1 -> pending entries
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    const history = useHistory()
+
+
+    useEffect(() => {
+        api.get('orphanages')
+            .then(response => {
+                setOrphanages(response.data);
+            });
+    }, [orphanages]);
+
+    function editOrphanage(id: number) {
+        history.push(`/orphanage/edit/${id}`)
+    };
+
+    async function deleteOrphanage(id: number) {
+        await api.delete('/orphanage', { id } as AxiosRequestConfig);
+    };
+
+
+
+    function switchContainer() {
+        if (orphanagesContainer === 0) {
+            setOrphanagesContainer(1);
+            return;
+        };
+
+        setOrphanagesContainer(0);
+    };
+
+    function logout() {
+        localStorage.removeItem('token');
+        setUser!(undefined);
+        history.push('/');
+    };
+
     return (
         <Container>
             <Sidebar.FixedContainer>
                 <Sidebar.Logo />
+                <Sidebar.GroupIcon>
+                    <Sidebar.Icon isActive={orphanagesContainer === 0 ? true : false} type="button" onClick={switchContainer}>
+                        <FiMapPin size={24} />
+                    </Sidebar.Icon>
+                    <Sidebar.Icon isActive={orphanagesContainer === 1 ? true : false} type="button" onClick={switchContainer}>
+                        <FiAlertCircle size={24} />
+                    </Sidebar.Icon>
+                </Sidebar.GroupIcon>
+                <Sidebar.Icon isActive={false} type="button" onClick={logout}>
+                    <FiPower size={24} />
+                </Sidebar.Icon>
             </Sidebar.FixedContainer>
-            Dashboard
+
+            <Content>
+                {orphanagesContainer === 0 ?
+                    (
+                        <OrphanagesContainer>
+                            <TitleContainer>
+                                <Title>
+                                    Orfanatos Cadastrados
+                                </Title>
+                                <OrphanagesTotal>{orphanages.length} orfanatos</OrphanagesTotal>
+                            </TitleContainer>
+                            <OrphanagesContent>
+                                {orphanages.map(orphanage => (
+                                    <MiniMap
+                                        key={orphanage.id}
+                                        name={orphanage.name}
+                                        latitude={orphanage.latitude}
+                                        longitude={orphanage.longitude}
+                                        Edit={() => editOrphanage(orphanage.id)}
+                                        Delete={() => deleteOrphanage(orphanage.id)}
+                                    />
+                                ))}
+                            </OrphanagesContent>
+                        </OrphanagesContainer>
+                    )
+                    :
+                    (
+                        <OrphanagesContainer>
+                            <TitleContainer>
+                                <Title>Cadastros pendentes</Title>
+                            </TitleContainer>
+                            <OrphanagesContent>
+                                {/* Use data */}
+                            </OrphanagesContent>
+                        </OrphanagesContainer>
+                    )
+                }
+            </Content>
+
         </Container>
     );
 };
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -5,3 +5,45 @@ export const Container  = styled.div`
     flex: 1;
     
 `;
+
+export const Content = styled.div`
+    flex: 1;
+    margin-top: 7rem;
+    margin-left: 11.2rem;
+`;
+
+export const OrphanagesContainer = styled.div`
+    display: flex; 
+    flex-direction: column;
+    flex: 1;
+`;
+
+export const TitleContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 8rem 1rem 8rem;
+    padding-bottom: 2rem;
+    border-bottom: solid .1rem #D3E2E5;
+`;
+
+export const Title = styled.h1`
+    font-size: 3.2rem;
+    font-weight: 700;
+    line-height: 3.4rem;
+    color: #4D6F80;
+`;
+
+export const OrphanagesTotal = styled.p`
+    color: #8FA7B2;
+    font-size: 1.6rem;
+    line-height: 2.2rem;
+`;
+
+export const OrphanagesContent = styled.div`
+    display: flex;
+    flex: 1;
+    flex-wrap: wrap;
+    margin: 2rem 8rem;
+    justify-content: center;
+`;
