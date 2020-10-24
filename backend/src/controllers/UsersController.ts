b15be5f0000f74d017192ecd754b92d1da4a94b2
jonasdevzero <jonasdevzero@gmail.com>
2020-10-24 14:45:14 -0300

    Improving UsersController.ts

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -23,13 +23,45 @@ function generateToken(params: object) {
 
 export default {
     async index(req: Request, res: Response) {
-        const usersRepository = getRepository(User);
+        try {
+            const usersRepository = getRepository(User);
+
+            const users = await usersRepository.find();
+
+            return res.status(200).json({ user: UserView.renderMany(users) });
+        } catch (err) {
+            console.log('[Error on index users] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        };
+    },
+
+    async show(req: Request, res: Response) {
+        const { user: { id } } = req.body;
 
         try {
-            const users = await usersRepository.find();
-            return res.send(UserView.renderMany(users));
+            const usersRepository = getRepository(User);
+
+            const user = await usersRepository.findOne({ id });
+
+            return res.status(200).json({ user: UserView.render(user as User) });
         } catch (err) {
-            return res.status(500).json({ error: 'Error on server' })
+            console.log('[Error on show user] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        };
+    },
+
+    async delete(req: Request, res: Response) {
+        const { id } = req.body;
+
+        try {
+            const usersRepository = getRepository(User);
+
+            const userDeleted = await usersRepository.delete(id);
+
+            return res.status(200).json({ user: userDeleted });
+        } catch (err) {
+            console.log('[Error on delete user] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' })
         };
     },
 
@@ -39,47 +71,35 @@ export default {
             email,
             password
         } = req.body;
-        const usersRepository = getRepository(User);
-
-        const existsUser = await usersRepository.findOne({ email });
-
-        if (existsUser) {
-            return res.status(400).json({ error: 'user already exists' });
-        }
-
-        const data = {
-            name,
-            email,
-            password: encryptPassword(password),
-        };
-
-        const schema = Yup.object().shape({
-            name: Yup.string().required(),
-            email: Yup.string().required(),
-            password: Yup.string().required()
-        });
-
-        await schema.validate(data, {
-            abortEarly: false,
-        });
-
-        const user = usersRepository.create(data);
-        await usersRepository.save(user);
-
-        return res.status(200).json({
-            token: generateToken({ id: user.id }),
-        })
-    },
-
-    async show(req: Request, res: Response) {
-        const { user: { id } } = req.body;
-        const usersRepository = getRepository(User);
 
         try {
-            const user = await usersRepository.findOne({ id });
-            return res.status(200).json({ user: UserView.render(user as User) });
+            const usersRepository = getRepository(User);
+
+            const existsUser = await usersRepository.findOne({ email });
+            if (existsUser) return res.status(400).json({ error: 'User already exists' });
+
+            const data = {
+                name,
+                email,
+                password: encryptPassword(password),
+            };
+
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                email: Yup.string().required(),
+                password: Yup.string().required()
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+
+            const user = usersRepository.create(data);
+            await usersRepository.save(user);
+
+            return res.status(200).json({ token: generateToken({ id: user.id }) });
         } catch (err) {
-            return res.status(500).json({ error: 'error on server' })
+            console.log('[Error on create user] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
         };
     },
 
@@ -89,46 +109,39 @@ export default {
             password
         } = req.body;
 
-        const usersRepository = getRepository(User);
+        try {
+            const usersRepository = getRepository(User);
 
-        const user = await usersRepository.findOne({ email });
+            const user = await usersRepository.findOne({ email });
 
-        if (!user)
-            return res.json({ error: 'User not found' });
+            if (!user)
+                return res.json({ error: 'User not found' });
 
-        if (!bcrypt.compareSync(password, user.password))
-            return res.json({ error: 'Invalid password' })
+            if (!bcrypt.compareSync(password, user.password))
+                return res.json({ error: 'Invalid password' });
 
-        return res.status(200).json({
-            user: UserView.render(user),
-            token: generateToken({ id: user.id }),
-        });
+            return res.status(200).json({
+                user: UserView.render(user),
+                token: generateToken({ id: user.id }),
+            });
+        } catch (err) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        };
     },
 
     async auth(req: Request, res: Response, next: NextFunction) {
         const { token } = req.body;
 
-        if (!token)
-            return res.status(401).json({ error: 'Access Denied' })
+        if (!token) return res.status(401).json({ error: 'Access Denied' });
 
         try {
             const verified = jwt.verify(token, tokenSecret)
+
             req.body.user = verified;
+
             next();
         } catch (err) {
-            return res.status(400).json({ error: 'Invalid Token' })
-        }
-    },
-
-    async delete(req: Request, res: Response) {
-        const { id } = req.body;
-        const usersRepository = getRepository(User);
-
-        try {
-            const userDeleted = await usersRepository.delete(id);
-            return res.send(userDeleted);
-        } catch (err) {
-            return res.status(500).json({ error: 'Error on server' })
+            return res.status(400).json({ error: 'Invalid Token' });
         };
     },
 };
