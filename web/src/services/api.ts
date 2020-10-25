449768560d04ad516346e4c699cb7c4213900bb8
jonasdevzero <jonasdevzero@gmail.com>
2020-10-25 15:47:30 -0300

    config web deploy

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://happy-backend-devzero.herokuapp.com',
 });
 
 export default api;
